9c58c8ffb7e905a056e7d4b8694673c84684986a
rsbnhyd <rsbnhyd@gmail.com>
2020-02-13 13:29:30 +0530

    my-local changes

--- Program.cs
+++ Program.cs
@@ -7,6 +7,7 @@ namespace merging
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Welcome to C#!");
         }
     }
 }
